d81f5814e060c5fdd4bcb9fbc0922aae0f13bf8e
Kaz <takaik@yahoo.com>
2022-03-10 11:19:00 -0500

    add solution to euler4.rb

--- euler4.rb
+++ euler4.rb
@@ -2,3 +2,20 @@
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+x = 100
+y = 100
+p_num = []
+
+while y < 1000
+  while x < 1000
+    z = x * y
+    if z.to_s == z.to_s.reverse
+      p_num << z
+    end
+    x += 1
+  end
+  x = 100
+  y += 1
+end
+
+p p_num.max
