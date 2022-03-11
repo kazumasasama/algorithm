6945765e8991d87e4872039bd261b3b0b3854e47
Kaz <takaik@yahoo.com>
2022-03-10 21:10:29 -0500

    create euler6.rb

---
+++ euler6.rb
@@ -0,0 +1,3 @@
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
