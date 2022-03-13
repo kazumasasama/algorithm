04f73b934f8d68b46b55032e694174bf135161cb
Kaz <takaik@yahoo.com>
2022-03-13 13:48:26 -0400

    add solution

--- euler9.rb
+++ euler9.rb
@@ -5,3 +5,20 @@
 
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
+a, b, c = 1, 1, 1
+while a <= 998
+  while b <= 998
+    while c <= 998
+      if a > 0 && b > 0 && c > 0 && a < b && b < c && a + b + c == 1000 && a ** 2 + b ** 2 == c ** 2
+        puts "a: #{a}, b: #{b}, c: #{c}"
+        puts a * b * c
+        return
+      end
+      c += 1
+    end
+    c = 0
+    b += 1
+  end
+  b = 0
+  a += 1
+end
